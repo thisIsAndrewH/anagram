9ce7e1b7dd33b8134ff15cbb6a4adaf8a623c609
Andrew Harris <aharris86@gmail.com>
2017-10-05 13:11:31 -0400

    cleanup

--- anagram/ViewController.swift
+++ anagram/ViewController.swift
@@ -24,24 +24,6 @@ class ViewController: UIViewController {
     @IBOutlet weak var anagramOutput: UITextView!
     
     @IBAction func findAnagrams(_ sender: Any) {
-        /*
-         * grab input: DONE
-         * store input: DONE
-         * sort input for quicker comparison: Done
-         * take the input and search against dictionary: Done
-         * load dictionary
-         * sort characters for each word: DONE
-         * compare input to dictionary until 0 or more results are found: DONE
-             * 0 = bummer
-             * >0 = list options
-         * display message: DONE
-         *
-         * assuming each word in dictionary is sorted, maintain the original index and keep trakc of those. use those indexes to find the original version of the words in the dictionary: DONE
-         * sort input a-z: DONE
-         * sort dictionary words a-z while maintaining existing indexing: DONE
-         */
-        
-        
         //grab the input
         let userInput = anagramInput.text
         
@@ -61,10 +43,7 @@ class ViewController: UIViewController {
     //The sorted bool will alphabatize each word for better/quick matching against the input word.
     private func loadDictionary(Sorted: Bool) -> Array<String> {
         let dictionaryWordsArray = fetchDictionary()
-            //["anarchic","anopheles","anti-federalist","aspirated","autocratship","abrogable","athanasy","athansay","athaansy"]
-        //TODO: Delete this print, it'll be huge when coming from a dictionary.
-        //print(dictionaryWordsArray)
-         if (Sorted == true){
+        if (Sorted == true){
             // For each item in the dictionary, create a new array for each letter, sort it, then convert back to a string, and add to the "sorted" array
             var dictionaryWordsArraySorted: [String] = []
             for word in dictionaryWordsArray {
@@ -72,8 +51,6 @@ class ViewController: UIViewController {
                 let newWord = String(tempArray) //re aggraigned word
                 dictionaryWordsArraySorted.append(newWord) //build out new array to use for matching
             }
-            //TODO: Delete this print, it'll be huge when coming from a dictionary.
-            //print(dictionaryWordsArraySorted)
             
             return dictionaryWordsArraySorted
         } else {
@@ -86,7 +63,6 @@ class ViewController: UIViewController {
         var jsonData: Data?
         if let file = Bundle.main.path(forResource: "dictionary_alpha_arrays", ofType: "json") {
             jsonData = try? Data(contentsOf: URL(fileURLWithPath: file))
-            
         } else {
             print("Fail")
         }
@@ -120,18 +96,16 @@ class ViewController: UIViewController {
                 wordMatches.append(count)
             }
             count = count + 1
-       }
-        print(wordMatches)
+        }
         
         //reconcile matches to original dictionary and return output
         if (wordMatches.isEmpty == false) {
             for index in wordMatches {
-                output += "\n" + dictionaryWordsArray[index]
+                output += "\n\n" + dictionaryWordsArray[index]
             }
         } else {
             output += "\n\nThere are no anagarams for your input. Please try again."
         }
-        print(output)
         
         return output
     }
